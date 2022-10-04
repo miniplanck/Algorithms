f6cc5ab1762ad86cf51adf3b0b6eb93e27038ea2
José Pereira <73555298+miniplanck@users.noreply.github.com>
2022-10-04 17:19:23 +0100

    Create Solution.js

---
+++ Pairwise/Solution.js
@@ -0,0 +1,24 @@
+ function pairwise(arr, arg) {
+  let sum = 0;
+  let used = {};
+
+  for (let i = 0; i<arr.length-1; i++){
+    for (let j = i+1; j<arr.length; j++){
+      if (arr[i]+arr[j] == arg){
+      
+        //Adds used indexes to the "used" object
+        if(!used.hasOwnProperty(i) && !used.hasOwnProperty(j)){
+          used[i] = true;
+          used[j] = true;
+        }
+        else {
+          continue;
+        }
+        sum += i+j
+      }
+    }
+  }
+
+  return sum;
+
+}
